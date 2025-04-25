648ede1c91b6f8adb973be4713d5087bf929a5d7
Dmitry Lotkov <dmitry.lotkov@gmail.com>
2025-04-25 22:03:28 +0300

    feat: add list.js task

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,21 @@
+import {fileURLToPath} from "url";
+import path from "path";
+import fs from "fs/promises";
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+
 const list = async () => {
-    // Write your code here 
+    const folderPath = path.join(dirname, 'files');
+
+    try {
+        await fs.access(folderPath);
+    } catch {
+        throw new Error('FS operation failed'); // folder doesnt exists
+    }
+
+    const files = await fs.readdir(folderPath);
+    files.forEach((file) => console.log(file));
 };
 
 await list();
