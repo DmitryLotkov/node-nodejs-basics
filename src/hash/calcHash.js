48645614af59ed182753a82898ec5c31ba97faaf
Dmitry Lotkov <dmitry.lotkov@gmail.com>
2025-04-27 15:27:46 +0300

    feat: add calcHash.js task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,17 @@
+import { createHash } from 'crypto';
+import { readFile } from 'fs/promises';
+import {fileURLToPath} from "url";
+import path from "path";
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+    const source = path.join(dirname, 'files/fileToCalculateHashFor.txt');
+
+    const fileBuffer = await readFile(source);
+    const hash = createHash('sha256').update(fileBuffer).digest('hex');
+    console.log(hash);
 };
 
 await calculateHash();
