fa49c89c6d5adcbf4b988cf6300ab3ea4246bcaa
Dmitry Lotkov <dmitry.lotkov@gmail.com>
2025-04-27 14:32:00 +0300

    feat: add esm.mjs task

--- src/modules/esm.mjs
+++ src/modules/esm.mjs
@@ -1,22 +1,28 @@
-const path = require('path');
-const { release, version } = require('os');
-const { createServer: createServerHttp } = require('http');
-require('./files/c');
+import path, { dirname } from 'path';
+import { release, version } from 'os';
+import { createServer as createServerHttp } from 'http';
+import { fileURLToPath } from 'url';
+import './files/c.cjs';
+import aJSON from './files/a.json' with { type: "json" };
+import bJSON from './files/b.json' with { type: "json" };
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 
 const random = Math.random();
 
 let unknownObject;
 
 if (random > 0.5) {
-    unknownObject = require('./files/a.json');
+    unknownObject = aJSON;
 } else {
-    unknownObject = require('./files/b.json');
+    unknownObject = bJSON;
 }
 
 console.log(`Release ${release()}`);
 console.log(`Version ${version()}`);
 console.log(`Path segment separator is "${path.sep}"`);
-
 console.log(`Path to current file is ${__filename}`);
 console.log(`Path to current directory is ${__dirname}`);
 
@@ -33,8 +39,4 @@ myServer.listen(PORT, () => {
     console.log('To terminate it, use Ctrl+C combination');
 });
 
-module.exports = {
-    unknownObject,
-    myServer,
-};
-
+export { unknownObject, myServer };
